d120eb66956a43a84cef34d209c1d785f9f8aa65
dld007 <48293479+dld007@users.noreply.github.com>
2020-06-12 16:15:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,18 +29,9 @@ var Arbit = {
   "sharkwing": true
 };
 
-//Log in
-const client = new Discord.Client()
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-//Set variables
-var now = new Date();
-var outputString = "";
-
-setInterval(function() {
-  //Request arbitration data
+//Function to retrieve arbitration data from server
+//Returns a string with data description
+function getWarframeData() {
   axios.get(`${WARFRAME_API_URL}/${platform}/arbitration`, { headers:
   WARFRAME_API_REQUEST_HEADERS })
     .then(response => {
@@ -49,19 +40,34 @@ setInterval(function() {
   })
   .catch(error => console.error('On get error',error))
   now = new Date();
-}, 3500000); //Repeats every 3,500,000 ms
 
+  var oString = "The current arbitration is a " + Arbit.type + " mission in " +
+    Arbit.node + " with enemy " + Arbit.enemy + " and expires at " + (now.getUTCHours()-3) + ":00 EST";
+
+  return oString;
+}
+
+//Log in
+const client = new Discord.Client()
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+//If a user sends "$arbitration" into a channel, it returns the arbitration data and then every
+//hour afterwards, the bot sends the new arbitration data
 client.on('message', function(message) {
-    outputString = "The current arbitration is a " + Arbit.type + " mission in " +
-      Arbit.node + " with enemy " + Arbit.enemy + " and expires at " + (now.getUTCHours()-3) + ":00 EST";
-  
-    //Initialization message - the arbitration message gets sent in the channel this init. message gets sent
+    // Now, you can use the message variable inside
     if (message.content === "$arbitration") {
+        outputString = getWarframeData();
+        message.channel.send(outputString)
+        .catch(error => console.error('On get error',error));
+
         var interval = setInterval (function () {
+            outputString = getWarframeData();
             // use the message's channel (TextChannel) to send a new message
             message.channel.send(outputString)
             .catch(error => console.error('On get error',error));
-        }, 3600000); //Repeats every hour
+        }, 3600000);
     }
 });
 
