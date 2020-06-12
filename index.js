a5d28bfb5003a935c55fc622034df1fc4795ae08
dld007 <48293479+dld007@users.noreply.github.com>
2020-06-12 15:54:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Discord = require("discord.js")
 //Get warframe worldstatedata library to access api
 const worldstateData = require('warframe-worldstate-data');
 const WARFRAME_API_URL = 'https://api.warframestat.us';
-const platform = 'PC';
+const platform = 'pc';
 const arbit = "arbitration"
 
 const WARFRAME_API_REQUEST_HEADERS = {
@@ -20,26 +20,14 @@ const WARFRAME_API_REQUEST_HEADERS = {
 
 //Set up arbitration variable
 var Arbit = {
-  activation:'',
-  expiry: '',
-  node: '',
-  enemy: '',
-  type: '',
-  archwing: true,
-  sharkwing: true
+  "activation": "2020-06-11T23:54:05Z",
+  "expiry": "2020-06-11T23:54:05Z",
+  "node": "string",
+  "enemy": "Orokin",
+  "type": "string",
+  "archwing": true,
+  "sharkwing": true
 };
-
-//Request arbitration data
-axios.get(`${WARFRAME_API_URL}/${platform}/arbitration`, { headers:
-WARFRAME_API_REQUEST_HEADERS })
-  .then(response => {
-  Arbit = response.data;
-})
-.catch(error => console.error('On get error',error))
-
-var now = new Date();
-console.log(now.getUTCMinutes());
-console.log(now.getUTCHours());
 
 //Log in
 const client = new Discord.Client()
@@ -47,13 +35,34 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-//If user sends !Arbit, send arbitration data
-client.on("message", msg => {
-  if (msg.content === "!Arbitration") {
-    msg.reply("The current arbitration is in " + Arbit.node + " with enemy "
-      + Arbit.enemy + " and expires at " + (now.getUTCHours()-3) + ":00 EST");
-  }
-})
+//Set variables
+var now = new Date();
+var outputString = "";
+
+setInterval(function() {
+  //Request arbitration data
+  axios.get(`${WARFRAME_API_URL}/${platform}/arbitration`, { headers:
+  WARFRAME_API_REQUEST_HEADERS })
+    .then(response => {
+    Arbit = response.data;
+    console.log(response.data);
+  })
+  .catch(error => console.error('On get error',error))
+  now = new Date();
+}, 3500000); //Repeats every 3,500,000 ms
+
+client.on('message', function(message) {
+    outputString = "The current arbitration is a " + Arbit.type + " mission in " +
+      Arbit.node + " with enemy " + Arbit.enemy + " and expires at " + (now.getUTCHours()-3) + ":00 EST";
+  
+    //Initialization message - the arbitration message gets sent in the channel this init. message gets sent
+    if (message.content === "$arbitration") {
+        var interval = setInterval (function () {
+            // use the message's channel (TextChannel) to send a new message
+            message.channel.send(outputString)
+            .catch(error => console.error('On get error',error));
+        }, 3600000); //Repeats every hour
+    }
+});
 
 client.login(process.env.BOT_TOKEN)
-
